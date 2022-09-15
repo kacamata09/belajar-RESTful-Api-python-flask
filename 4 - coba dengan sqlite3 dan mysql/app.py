63897d8b74580726b086fff218d18d05a883f7a8
kacamata09 <muh.ansharazhari@gmail.com>
2022-09-15 14:31:47 +0700

    menyelesaikan restful sqlite dan mysql

--- 4 - coba dengan sqlite3 dan mysql/app.py
+++ 4 - coba dengan sqlite3 dan mysql/app.py
@@ -1,4 +1,3 @@
-from urllib import request
 from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api, Resource
@@ -8,23 +7,54 @@ aplikasiku = Flask(__name__)
 apiku = Api(aplikasiku)
 CORS(aplikasiku)
 
-mahasiswa = {}
+# database
+# aplikasiku.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/cobarestful'
+aplikasiku.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dbcobarestful.db'
+dbku = SQLAlchemy(aplikasiku)
+
+class Mahasiswa(dbku.Model):
+    nim = dbku.Column(dbku.String(12), primary_key=True)
+    nama = dbku.Column(dbku.String(100), nullable=False)
+    jurusan = dbku.Column(dbku.String(50))
+    ipk = dbku.Column(dbku.Integer)
+    
+    def __repr__(self) -> str:
+        return f'<{self.nama}>'
+    
+    def save(self):
+        try:
+            dbku.session.add(self)
+            dbku.session.commit()
+            return True
+        except:
+            return False
+
 
 class IniResource(Resource):
     def get(self):
-        return mahasiswa
+        dataMahasiswa = Mahasiswa.query.all()
+        tampilkan = [{
+          'nim': data.nim,
+          'nama': data.nama,
+          'jurusan': data.jurusan,
+          'ipk': data.ipk 
+        } for data in dataMahasiswa
+                     ]
+        return tampilkan
     
     def post(self):
-        nama = request.form['nama']
-        ipk = int(request.form['ipk'])
         nim = request.form['nim']
-        mahasiswa['nama'] = nama
-        mahasiswa['ipk'] = ipk
-        mahasiswa['nim'] = nim
-        return ['alhamdulillah msuk bro']
+        nama = request.form['nama']
+        jurusan = request.form['jurusan']
+        ipk = int(request.form['ipk'])
+        mahasiswaBaru = Mahasiswa(nim=nim, nama=nama, jurusan=jurusan, ipk=ipk)
+        mahasiswaBaru.save()
+        
+        return ['alhamdulillah masuk bro']
         
 
 
 apiku.add_resource(IniResource, '/apiku', methods=['GET', 'POST'])
 
-aplikasiku.run(debug=True)
+if __name__ == '__main__':
+    aplikasiku.run(debug=True)
