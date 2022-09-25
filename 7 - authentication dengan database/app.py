5e2ee1afa94ff42f25b3ee347cf2ebfb5de27a93
kacamata09 <muh.ansharazhari@gmail.com>
2022-09-25 23:53:14 +0700

    tambah flask-login

--- 7 - authentication dengan database/app.py
+++ 7 - authentication dengan database/app.py
@@ -1,3 +1,4 @@
+from enum import unique
 from flask import Flask, request, jsonify, make_response
 from flask_restful import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
@@ -5,6 +6,9 @@ from datetime import datetime, timedelta
 from werkzeug.security import generate_password_hash, check_password_hash
 import jwt
 from flask_migrate import Migrate
+from flask_login import UserMixin, current_user, login_required, \
+    login_user, logout_user, LoginManager
+
 
 from functools import wraps
 
@@ -16,6 +20,17 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///belajarrest.db'
 apiku = Api(app)
 dbku = SQLAlchemy(app)
 migrasidb = Migrate(app, dbku)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'loginuser'
+
+@login_manager.user_loader
+def load_user(id):
+    try:
+        return Pengguna.query.get_or_404(int(id))
+    except:
+        return jsonify({'pesan':'anda gagal login'})
 
 def token_required(fungsi):
     @wraps(fungsi)
@@ -49,9 +64,10 @@ def akses_admin(fungsi):
     return kunci_halaman
 
 
-class Pengguna(dbku.Model):
+class Pengguna(dbku.Model, UserMixin):
     __tablename__ = 'flask-pengguna'
-    username = dbku.Column(dbku.String(50), primary_key=True)
+    id = dbku.Column(dbku.Integer, primary_key=True, nullable=False)
+    username = dbku.Column(dbku.String(50), unique=True)
     password_hash = dbku.Column(dbku.String(255))
     hak_akses = dbku.Column(dbku.String(50), default='user')
     
@@ -65,7 +81,8 @@ class Pengguna(dbku.Model):
     
     def cek_password(self, password_hash):
         return check_password_hash(self.password_hash, password_hash)
-    
+
+
     
 
 class Register(Resource):
@@ -83,21 +100,26 @@ class Register(Resource):
                 dbku.session.add(penggunaBaru)
                 dbku.session.commit()
             except:
+                # dbku.session.add(penggunaBaru)
+                # dbku.session.commit()
                 return jsonify({'pesan':'kemungkinan ada yang salah pada database'})
         elif getPengguna != None:
             return jsonify({'pesan':'sudah ada username yang sama disini, silahkan cari username baru'})
         return jsonify({'pesan':f'selamat anda telah berhasil register, saudara / saudari {username}. silahkan login untuk mendapatkan token'})
     
-class Login(Resource):
+class LoginUser(Resource):
         def post(self):
-            username = request.form.get('username').lower()
+            # id = request.form.get('id')
             # print(username)
+            username = request.form.get('username')
             password = request.form.get('password')
             try:
-                getPengguna = Pengguna.query.get_or_404(username)
+                # getPengguna = Pengguna.query.get_or_404(id)
+                getPengguna = Pengguna.query.filter_by(username=username).first()
             except:
                 return jsonify({'pesan':'username yang anda masukkan nggk ada bro'})
             if getPengguna.cek_password(password):
+                login_user(getPengguna)
             # if getPengguna.password_hash == password:
                 token = jwt.encode(
                     {'username': username, 'exp':datetime.utcnow() + timedelta(days=1)}, 
@@ -105,9 +127,20 @@ class Login(Resource):
                     algorithm='HS256')
                 return jsonify({'pesan':'anda berhasil login, silahkan copy tokennya untuk authentikasi', 'token':token})
             return jsonify({'pesan':'coba lagi mungkin password anda salah'})
+        
+        def get(self):
+            return make_response(jsonify({'pesan':'login dulu bro'}))
+        
+        def delete(self):
+            semua_user = Pengguna.query.all()
+            for user in semua_user:
+                dbku.session.delete(user)
+                dbku.session.commit()
+            return jsonify({'pesan':'semua sudah terhapus'})
 
 class Dashboard(Resource):
     @token_required
+    # @login_required
     def get(self):
         return jsonify({'pesan':'anda berhasil masuk menggunakan token'})
 
@@ -116,19 +149,21 @@ class BebasArea(Resource):
         return jsonify({'pesan':'ini adalah halaman bebas akses. Hacker juga boleh masuk'})
     
 class HalamanAdmin(Resource):
-    @token_required
-    @akses_admin
+    @login_required
+    # @token_required
+    # @akses_admin
     def get(self):
         return jsonify({'pesan':'selamat anda berhasil login berarti anda admin'})
 
 
 
 apiku.add_resource(Register, '/api/register/', methods=['POST'])
-apiku.add_resource(Login, '/api/login/', methods=['POST'])
-apiku.add_resource(Dashboard, '/api/dashboard/', methods=['GET'])
+apiku.add_resource(LoginUser, '/api/login/', methods=['POST', 'GET', 'DELETE'])
+apiku.add_resource(Dashboard, '/api/dashboard/', methods=['GET', 'POST'])
 apiku.add_resource(BebasArea, '/bebas/', methods=['GET'])
 apiku.add_resource(HalamanAdmin, '/admin/', methods=['GET'])
 
 if __name__ == '__main__':
     app.run(debug=True)
     # dbku.create_all()
+    
